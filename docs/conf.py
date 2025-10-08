30b96635f4a1c8e08b605e5c7e5171243156d9bd
Silver <71855839+shockingfargroundx@users.noreply.github.com>
2025-10-08 15:12:11 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,11 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme',]
+extensions = ['sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',]
 
 
 templates_path = ['_templates']
