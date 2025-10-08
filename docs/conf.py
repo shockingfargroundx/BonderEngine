f9b3206f1f7668d93a85e859a479e5e08fa01c99
Silver <71855839+shockingfargroundx@users.noreply.github.com>
2025-10-08 15:02:35 -0700

    eh

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme',]
+html_theme = "sphinx_rtd_theme"
 
 templates_path = ['_templates']
 exclude_patterns = []
